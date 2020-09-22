75a03e4ae0f231bc58f9f15ab65f23956e7a736b
Matrixoid <40433531+Matrixoid@users.noreply.github.com>
2020-09-23 01:16:08 +0300

    lab1

--- 6.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-grep -e "\\(WW\\)" /var/log/anaconda/X.log | sed -e "s/\\(WW\\)/Warning: /" > full.log
-grep -e "\\(II\\)" /var/log/anaconda/X.log | sed -e "s/\\(II\\)/Information: /" >> full.log
