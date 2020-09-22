e37584a8e3896b9a2825758899efd935d0c0d2df
M3133_Chmyhalov_Artemij <284276@niuitmo.ru>
2020-09-23 00:55:39 +0300

    lab1

---
+++ lab1/i.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $1 -gt $2 ]]
+then
+    a=$1
+else
+    a=$2
+fi
+
+if [[ $3 -gt "$a" ]]
+then
+    b=$3
+else
+    b=$a
+fi
+
+echo $b
---
+++ lab1/ii.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+res=""
+str=""
+read str
+while [[ $str != "q" ]]
+do
+    res+=$str
+    read str;
+done
+
+echo $res
