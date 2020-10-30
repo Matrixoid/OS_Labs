b9d773096f38207a65ea5335c1848594a8773d37
M3133_Chmyhalov_Artemij <284276@niuitmo.ru>
2020-10-31 00:01:15 +0300

    1

--- lab3/1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir ~/test &&  {
-									echo "catalog wac created successfully" >> ~/report
-									touch ~/test/$(date +'%d-%m-%y_%H:%M:%S')
-								 } 
-ping -c 1 www.net_nikogo.ru || echo "Error! Host is not available!" >> ~/report
